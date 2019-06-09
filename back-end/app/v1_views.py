a0b0b2f06080b081c13863e6fcfb7eb0ade7cc0e
Vincent Asante <vincenthokie@gmail.com>
2019-06-09 21:27:46 +0300

    - add type add & view routes - check if type is valid before creating menu

--- back-end/app/v1_views.py
+++ back-end/app/v1_views.py
@@ -73,14 +73,30 @@ def add_to_menu():
     if form.validate_on_submit():
 
         # create the list
-        menu = Menu(form.name.data, session["user"])
+        menu = Menu(
+            form.name.data,
+            form.price.data,
+            form.photo.data,
+            form.type.data)
+
+        #check that type exists
+        type = Type.query.filter_by(type_id=form.type.data).first()
+        if not type:
+            response = jsonify(
+                {
+                    "error":
+                    "That type ('" + str(form.type.data) + "') does not exist menu"
+                })
+            response.status_code = 400
+
+            return response
 
         try:
             menu.save()
 
             # retrieve the list and send it back to the user
             menu = Menu.query.filter_by(
-                name=form.name.data, user_id=session["user"]).first()
+                name=form.name.data, type_id=form.type.data).first()
             response = jsonify(menu.serialize)
             response.status_code = 201
 
@@ -102,12 +118,62 @@ def add_to_menu():
         return response
 
 
+@app.route("/v1/type", methods=['POST'])
+def add_to_menu_type():
+
+    form = TypeForm()
+
+    # the form was properly filled
+    if form.validate_on_submit():
+
+        # create the list
+        type = Type(form.name.data)
+
+        try:
+            type.save()
+
+            # retrieve the list and send it back to the user
+            type = Type.query.filter_by(
+                name=form.name.data).first()
+            response = jsonify(type.serialize)
+            response.status_code = 201
+
+        except exc.IntegrityError:
+            response = jsonify(
+                {
+                    "error":
+                    "The type : '" + form.name.data +
+                    "' already exists, please change the name"
+                })
+            response.status_code = 406
+
+        return response
+
+    # the form was not properly filled, return an error message
+    else:
+        response = jsonify({"error": form.errors})
+        response.status_code = 422
+        return response
+
+
 @app.route("/v1/menu", methods=['GET'])
 def get_menu_list():
 
     # retrieve and send back the needed information
     response = jsonify([
         i.serialize for i in Menu.get_all()
+    ])
+
+    response.status_code = 200
+    return response
+
+
+@app.route("/v1/type", methods=['GET'])
+def get_type_list():
+
+    # retrieve and send back the needed information
+    response = jsonify([
+        i.serialize for i in Type.get_all()
     ])
 
     response.status_code = 200
